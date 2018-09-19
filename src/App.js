7d9fe155fde3e6584ad23acb08a5b481cc6f0f19
Kristen Payne <motorwolf@motorwolf.attlocal.net>
2018-09-19 12:30:09 -0700

    Code is working with some problems

--- src/App.js
+++ src/App.js
@@ -10,27 +10,24 @@ class App extends Component {
   }
   
   subtractor = (num) => {
-    while(num > 25){
+    while(num >= 26){
       //console.log(num);
-      num -= 25;
+      num -= 26;
     }
     return num;
   }
 
   letterShift = (event) => {
-    let textInput = event.target.value;
-    let textLen = textInput.length;
-    let newLetter = textInput[textLen - 1]; // will need to split this off too. this takes the last letter of the input, the one we need to convert.
-    //let translation = this.state.translated + newLetter; 
-    //this.setState({translated: translation});
-    //console.log(this.state.translation);
-    let currIndex = cypher.indexOf(newLetter.toLowerCase());
-    if(currIndex === -1){
-      this.setState({entry:textInput});
+    let textInput = event.target.value; // this takes our input entirely
+    let textLen = textInput.length; // just a shortcut for getting the length ... 15
+    console.log(textLen);
+    let newLetter = textInput[textLen - 1]; // this takes the very last letter entered, which is our target for translation. the rest of the string has already been translated ... a
+    let currIndex = cypher.indexOf(newLetter.toLowerCase()); // this is the index of the newly entered letter in cypher ... 0 
+    if(currIndex === -1){ // if it is not a letter, don't mess with it.
+      this.setState({entry:textInput}); // just returns the current string
     } else {
-      let restOfString = textInput.substring(0,(textLen - 1));
-      //console.log(restOfString);
-      let newIndex = currIndex + 5 + textLen;
+      let restOfString = textInput.substring(0,(textLen - 1)); // this takes the rest of the string minus our newly entered letter.
+      let newIndex = currIndex + 5 + textLen; // 0 +5 + 15 = 20 index is u
       this.setState({entry: restOfString + cypher[this.subtractor(newIndex)]});
       //console.log(this.subtractor());
       //let translatedLetter = cypher[this.subtractor(textLen) - 5];
@@ -39,12 +36,17 @@ class App extends Component {
   }
   translator = (event) => {
     let toBeTranslated = this.state.entry.split("").map((letter,inx) => {
-      let cyphIndex = cypher.indexOf(letter);
+      let cyphIndex = cypher.indexOf(letter); // g. 27 word length
       if(cyphIndex === -1){
         return letter;
       } else {
-        let len = inx + 6 + cyphIndex;
-        return cypher[this.subtractor(len)];
+        let len = (cyphIndex - 5) - (inx + 1); //1 - 28 = -27
+        while(len < 0){
+          len += cypher.length;
+        }
+        console.log(len);
+        //return cypher[this.subtractor(len)];
+        return cypher[len];
       }
     }).join("");
     this.setState(
